4d665e284b16552992e6ec82e8593c10ffc6f836
agp <aagamanpokharel45@gmail.com>
2026-05-26 22:59:28 +0530

    Added config for agent

---
+++ backend/README.md
@@ -0,0 +1,1 @@
+# THIS IS THE DOC for backend!
---
+++ backend/drift_agent/config.py
@@ -0,0 +1,8 @@
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
+OPENROUTER_BASE_URL = "https://openrouter.ai/api/v1"
+OPENROUTER_MODEL = os.getenv("OPENROUTER_MODEL","deepseek/deepseek-v4-flash:free")
+
